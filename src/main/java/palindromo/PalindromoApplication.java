0559c71ee0af2ad71e3849dfcb1c679379cabe80
Arguellis17 <arguelloj866@gmail.com>
2025-03-23 13:08:35 -0500

    Se arreglaron carpetas

--- src/main/java/palindromo/PalindromoApplication.java
+++ src/main/java/palindromo/PalindromoApplication.java
@@ -1,9 +1,8 @@
-package com.palindromo.conocerPalindromo;
+package palindromo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.sql.SQLOutput;
 
 @SpringBootApplication
 public class PalindromoApplication {
--- src/main/java/palindromo/controllers/LogicaRestController.java
+++ src/main/java/palindromo/controllers/LogicaRestController.java
@@ -1,4 +1,4 @@
-package controllers;
+package palindromo.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
